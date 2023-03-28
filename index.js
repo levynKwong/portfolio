a6a8a6c7ea1769df9db4ccef832f87fa90e66054
levynkhs <levynkhs2001@gmail.com>
2023-03-29 02:21:32 +0400

    toggle on placed

---
+++ index.js
@@ -0,0 +1,37 @@
+let isHidden = true;
+
+// hide the elements initially
+document.querySelectorAll('.middle-folder, .smile, .computer, .email, .note, .work').forEach(el => {
+  el.style.display = 'none';
+});
+
+const middleFolderClose = document.querySelector('#middle-folder-close');
+const middleFolder = document.querySelector('#middle-folder');
+
+middleFolderClose.addEventListener('click', () => {
+  if (isHidden) {
+    document.querySelectorAll('.middle-folder, .smile, .computer, .email, .note, .work').forEach(el => {
+      el.style.display = 'block';
+    });
+    middleFolderClose.classList.add('hide');
+    middleFolder.classList.remove('hide');
+    isHidden = false;
+  } else {
+    document.querySelectorAll('.middle-folder, .smile, .computer, .email, .note, .work').forEach(el => {
+      el.style.display = 'none';
+    });
+    middleFolderClose.classList.remove('hide');
+    middleFolder.classList.add('hide');
+    isHidden = true;
+  }
+});
+
+middleFolder.addEventListener('click', () => {
+  if (!isHidden) {
+    middleFolderClose.classList.remove('hide');
+    middleFolder.classList.add('hide');
+    isHidden = true;
+  }
+});
+
+
